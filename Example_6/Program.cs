46b19983352bd633cb98e64a401fcd3d94a5b10e
Nadezhda Saunanen <saunanen@yandex.ru>
2023-06-29 01:00:39 +0300

    Решение 6 задачи

---
+++ Example_6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");   
+double N = Convert.ToDouble(Console.ReadLine());
+if (N%2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
