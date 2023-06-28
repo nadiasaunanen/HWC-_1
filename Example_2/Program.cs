420a6efb6b0d422d709f732eda9f4c31478b0294
Nadezhda Saunanen <saunanen@yandex.ru>
2023-06-29 00:34:22 +0300

    Решение 2 задачи

---
+++ Example_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+double firstDigit = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double secondDigit = Convert.ToDouble(Console.ReadLine());
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine("Наибольшее число " + firstDigit + " Наименьшее число " + secondDigit);
+}
+else
+{
+    Console.WriteLine("Наибольшее число " + secondDigit + " Наименьшее число " + firstDigit);
+}
