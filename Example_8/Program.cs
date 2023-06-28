001dba00260fd54ba4a475dbcb2b8fbd78efe38c
Nadezhda Saunanen <saunanen@yandex.ru>
2023-06-29 01:29:10 +0300

    Решение 8 задачи

---
+++ Example_8/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+double number = Convert.ToDouble(Console.ReadLine());
+int count = 1;
+if (number < 1)
+{
+     Console.Write("Недопустимые значения");
+}
+while (count < number)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(count + ", ");
+        count++;
+    }
+    else
+    {
+        count++;
+    }
+}
